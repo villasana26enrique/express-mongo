996a487f0eb44b297836ffbbb6bb7ec69eb8b797
Enrique Villasana <enrique.villasana26@gmail.com>
2021-01-28 17:33:41 -0400

    Creating Connection

---
+++ conn.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const db_path = 'mongodb://localhost:27017';
+const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+mongoose.connect(db_path, config, error => {
+    if (!error) {
+        console.log('Succesful connection.')
+    } else {
+        console.log('Error Connecting to DB.')
+    }
+});
